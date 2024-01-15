24d958539bb764c0126e765d7b37a3f19d3243f8
Freya Pereira <freyapereira115@gmail.com>
2024-01-15 09:59:17 +0530

    commit add

--- project137/src/project137/add2nos.java
+++ project137/src/project137/add2nos.java
@@ -5,6 +5,10 @@ public class add2nos {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		int a=5 , b=4;
+		int c= a + b;
+		System.out.println("sum = "+c);
+		
 	}
 
 }
